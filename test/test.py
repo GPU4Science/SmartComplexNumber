e94dd14feec08a663d383bc235b5c8ac6176d191
Chivier Humber <chivier.humber@outlook.com>
2024-01-22 12:05:24 +0800

    update: update test case

--- test/test.py
+++ test/test.py
@@ -1,27 +1,51 @@
-import telekinesis
+import smartfp
 import numpy as np
 import time
 
-size = 100000000
-arr1 = np.linspace(1.0, 100.0, size)
-arr2 = np.linspace(1.0, 100.0, size)
+size = 1024
+arr1_real = np.linspace(1.0, 1.0, size)
+arr1_imag = np.linspace(1.0, 1.0, size)
+arr2_real = np.linspace(1.0, 10.0, size)
+arr2_imag = np.linspace(1.0, 10.0, size)
+device_cnt = 1
+level = 4
 
-runs = 100
-factor = 1.0001
+arr1_real_backup = arr1_real.copy()
+arr1_imag_backup = arr1_imag.copy()
+arr2_real_backup = arr2_real.copy()
+arr2_imag_backup = arr2_imag.copy()
+
+arr_cpu_real = np.zeros(size)
+arr_cpu_imag = np.zeros(size)
+
+runs = 1
 
 t0 = time.time()
-# telekinesis.multiply_with_scalar(arr1, 1.0001, 100)
 
-ptr = telekinesis.array_create(arr1)
+ptr1 = smartfp.smartfp_arr_create(arr1_real, arr1_imag, level, device_cnt)
+ptr2 = smartfp.smartfp_arr_create(arr2_real, arr2_imag, level, device_cnt)
 for run_index in range(runs):
-    telekinesis.array_map(ptr, factor, size)
-telekinesis.array_remove(ptr, arr1)
+    smartfp.smartfp_arr_compute(ptr1, ptr2)
+smartfp.smartfp_arr_fetch(ptr1, arr1_real, arr1_imag, level, device_cnt)
+smartfp.smartfp_arr_free(ptr1)
+smartfp.smartfp_arr_free(ptr2)
 
     
 print("gpu time: " + str(time.time()-t0))
 t0 = time.time()
 for _ in range(runs):
-    arr2 = arr2 * factor
+    for i in range(size):
+        arr_cpu_real[i] = arr1_real_backup[i]*arr2_real_backup[i] - arr1_imag_backup[i]*arr2_imag_backup[i]
+        arr_cpu_imag[i] = arr1_real_backup[i]*arr2_imag_backup[i] + arr1_imag_backup[i]*arr2_real_backup[i]
 print("cpu time: " + str(time.time()-t0))
 
-print("results match: " + str(np.allclose(arr1,arr2)))
+# print 10 items
+print("gpu results:")
+for i in range(10):
+    print(str(arr1_real[i]) + " + " + str(arr1_imag[i]) + "i")
+print("cpu results:")
+for i in range(10):
+    print(str(arr_cpu_real[i]) + " + " + str(arr_cpu_imag[i]) + "i")
+print("real results match: " + str(np.allclose(arr1_real, arr_cpu_real)))
+print("imag results match: " + str(np.allclose(arr1_imag, arr_cpu_imag)))
+
