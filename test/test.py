7beae9cdc256b1d551a12e00a75e159bffdb080c
Chivier Humber <chivier.humber@outlook.com>
2024-01-22 18:42:18 +0800

    update: update test case

--- test/test.py
+++ test/test.py
@@ -2,13 +2,13 @@ import smartfp
 import numpy as np
 import time
 
-size = 1024
+size = 1 << 21
 arr1_real = np.linspace(1.0, 1.0, size)
-arr1_imag = np.linspace(1.0, 1.0, size)
-arr2_real = np.linspace(1.0, 10.0, size)
-arr2_imag = np.linspace(1.0, 10.0, size)
+arr1_imag = np.linspace(-1.0, -1.0, size)
+arr2_real = np.linspace(1.0, 1.0, size)
+arr2_imag = np.linspace(-1.0, -1.0, size)
 device_cnt = 1
-level = 4
+level = 3
 
 arr1_real_backup = arr1_real.copy()
 arr1_imag_backup = arr1_imag.copy()
@@ -18,7 +18,7 @@ arr2_imag_backup = arr2_imag.copy()
 arr_cpu_real = np.zeros(size)
 arr_cpu_imag = np.zeros(size)
 
-runs = 1
+runs = 10
 
 t0 = time.time()
 
@@ -29,23 +29,32 @@ for run_index in range(runs):
 smartfp.smartfp_arr_fetch(ptr1, arr1_real, arr1_imag, level, device_cnt)
 smartfp.smartfp_arr_free(ptr1)
 smartfp.smartfp_arr_free(ptr2)
-
     
 print("gpu time: " + str(time.time()-t0))
+
+
 t0 = time.time()
 for _ in range(runs):
     for i in range(size):
         arr_cpu_real[i] = arr1_real_backup[i]*arr2_real_backup[i] - arr1_imag_backup[i]*arr2_imag_backup[i]
         arr_cpu_imag[i] = arr1_real_backup[i]*arr2_imag_backup[i] + arr1_imag_backup[i]*arr2_real_backup[i]
+    arr1_real_backup = arr_cpu_real.copy()
+    arr1_imag_backup = arr_cpu_imag.copy()
+    # for i in range(10):
+    #     print(str(arr_cpu_real[i]) + " + " + str(arr_cpu_imag[i]) + "i")
 print("cpu time: " + str(time.time()-t0))
 
 # print 10 items
-print("gpu results:")
-for i in range(10):
-    print(str(arr1_real[i]) + " + " + str(arr1_imag[i]) + "i")
-print("cpu results:")
-for i in range(10):
-    print(str(arr_cpu_real[i]) + " + " + str(arr_cpu_imag[i]) + "i")
-print("real results match: " + str(np.allclose(arr1_real, arr_cpu_real)))
-print("imag results match: " + str(np.allclose(arr1_imag, arr_cpu_imag)))
+# print("gpu results:")
+# for i in range(10):
+#     print(str(arr1_real[i]) + " + " + str(arr1_imag[i]) + "i")
+# print("cpu results:")
+# for i in range(10):
+#     print(str(arr1_real_backup[i]) + " + " + str(arr1_imag_backup[i]) + "i")
+
+for i in range(size):
+    if abs(arr1_real[i] - arr1_real_backup[i]) > 0.0001:
+        print("real error at index " + str(i) + ": " + str(arr1_real[i]) + " != " + str(arr1_real_backup[i]))
+    if abs(arr1_imag[i] - arr1_imag_backup[i]) > 0.0001:
+        print("imag error at index " + str(i) + ": " + str(arr1_imag[i]) + " != " + str(arr1_imag_backup[i]))
 
